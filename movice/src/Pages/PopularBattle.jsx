c201c78e63c718e1f6d41a16f62a6e82a6f1f85b
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-16 16:39:35 +0100

    function on clik

--- movice/src/Pages/PopularBattle.jsx
+++ movice/src/Pages/PopularBattle.jsx
@@ -1,11 +1,66 @@
 import React, { Component } from 'react'
+import Card from '../components/Card'
 
 export default class PopularBattle extends Component {
-    render() {
-        return (
-            <div>
-                popular battle
-            </div>
-        )
+    constructor(){
+        super()
+        this.state={
+            movies : [],
+            currentBattle : 0
+        }   
+        //bind
+        this.nextMovies = this.nextMovies.bind(this)
     }
-}
+    //method
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9958597c07ec90e02675c97691bf4506')
+            .then(res => res.json())
+            .then(result => {
+        this.setState({ movies: result.results })
+      })
+    }
+    nextMovies(currentBattle){
+       if(currentBattle<18){
+      this.setState({currentBattle: currentBattle + 2})
+      
+     }
+   }
+    
+
+    render() {
+        const { movies, currentBattle }=this.state
+        
+        return (
+            
+             <div className="container">
+          {this.state.movies.length !== 0 && 
+            <>
+              <Card 
+                title={movies[currentBattle].title} 
+                overview={movies[currentBattle].overview} 
+                release_date={movies[currentBattle].release_date}
+                posterPath={movies[currentBattle].poster_path}
+                currentBattle={currentBattle}
+                onClick={this.nextMovies}
+                
+                
+              />
+              <Card 
+                 title={movies[currentBattle + 1].title} 
+                overview={movies[currentBattle + 1].overview} 
+                release_date={movies[currentBattle + 1].release_date}
+                posterPath={movies[currentBattle + 1].poster_path}
+                currentBattle={currentBattle}
+                onClick={this.nextMovies}
+              />   
+            </>  
+          }
+           </div>
+        
+          
+     )
+  }
+
+}  
+  
+
--- movice/src/components/Card.jsx
+++ movice/src/components/Card.jsx
@@ -4,17 +4,20 @@ import '/Users/vins/Documents/konexio/react/movice/movice/src/styles/Card.css'
 
 export default class Card extends Component {
     render() {
-        const { movies ,title,release_date, overview,posterPath}= this.props
+        const { onClick,title,release_date, overview,posterPath, currentBattle}= this.props
         console.log(this.props);
         return (
                
-                    <div className='row align-items-start'>
-                        <div className='col-6'> 
-                            <img src={`https://image.tmdb.org/t/p/w300/${posterPath}`} alt="Présentation film" />
+                    <div  className='col-6'>
+                        <div className=''> 
+                            <img src={`https://image.tmdb.org/t/p/w300/${posterPath}`} 
+                            alt="Présentation film"
+                            onClick={() => onClick(currentBattle)}
+                            />
                         </div>
                         <h1>{title}</h1>
                         <h4>{release_date}</h4>
-                        <p>{overview}</p>
+                        <div><p>{overview}</p></div>
                     </div>
                
   
